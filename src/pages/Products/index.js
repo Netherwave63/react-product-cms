c6026078bf535438955dc6eeef524c77a2c8dad3
netherwave <netherwave63@gmail.com>
2020-03-01 23:09:05 +0800

    Update products page

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Products = () => {
+  const [productName, setProductName] = useState('')
+  const [packagingMaterial, setPackagingMaterial] = useState('Plastic bag')
+  const [packagingMethod, setPackagingMethod] = useState('Auto')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(productName, packagingMaterial, packagingMethod)
+  }
+
   return (
-    <ul>
-      <li>Cheese finger</li>
-      <li>Squid prawn paste</li>
-      <li>Love shape cheese tofu</li>
-      <li>Crabster ball</li>
-      <li>Seafood bean curd stick</li>
-    </ul>
+    <form onSubmit={handleSubmit}>
+      <div className='field'>
+        <label className='label'>Product name:</label>
+        <div className='control'>
+          <input 
+            className='input' 
+            onChange={(e) => setProductName(e.target.value)}
+            placeholder='Please specify your product name'
+            required={true}
+            value={productName}
+          />
+        </div>
+      </div>
+      <div className='field'>
+        <label className='label'>Packaging material:</label>
+        <div className='control'>
+          <div className='select'>
+            <select
+              onChange={(e) => setPackagingMaterial(e.target.value)}
+              value={packagingMaterial}
+            >
+              <option>Plastic bag</option>
+              <option>Film</option>
+              <option>Tray</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className='field'>
+        <label className='label'>Packaging method:</label>
+        <div className='control'>
+          <div className='select'>
+            <select
+              onChange={(e) => setPackagingMethod(e.target.value)}
+              value={packagingMethod}
+            >
+              <option>Auto</option>
+              <option>Manual</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <button className='button is-link' type='submit'>Confirm</button>
+    </form>
   )
 }
 
