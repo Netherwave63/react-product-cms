c57d3e82e2b37542de1ccc82f9fba8608ade838d
YZ.Chew <YZ.Chew@doo.hk>
2020-04-07 22:49:55 +0800

    Add orders unit carton

--- src/pages/Dashboard/Planning/AddNew.js
+++ src/pages/Dashboard/Planning/AddNew.js
@@ -7,11 +7,14 @@ const AddNew = ({
   // props
   customers,
   setIsActive,
+  // state
+  selectedProduct,
   // dispatch
   addOrder
 }) => {
   const [customerId, setCustomerId] = useState(customers[0]._id);
   const [customerOrders, setCustomerOrders] = useState('');
+  const [ordersUnit, setOrdersUnit] = useState('Carton');
 
   const handleClick = () => {
     setIsActive(false);
@@ -19,17 +22,29 @@ const AddNew = ({
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
+      e.preventDefault();
       if (!customerOrders) return;
       handleSubmit();
     }
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    let data;
+    if (ordersUnit === 'Carton') {
+      let productInfo = customers.find(customer => customer._id === customerId).products.find(product => product.product_id === selectedProduct);
+      data = {
+        customerId,
+        customerOrders: productInfo.package_per_carton * productInfo.weight_per_package * customerOrders
+      }; 
+    } else {
+      data = {
+        customerId,
+        customerOrders
+      };
+    }
     await addOrder({
       uuid: v4(),
-      customerId,
-      customerOrders
+      ...data
     });
     setIsActive(false);
   };
@@ -60,12 +75,26 @@ const AddNew = ({
               </div>
             </div>
             <div className='field'>
-              <label className='label'>Customer orders (kg):</label>
+              <label className='label'>Orders unit:</label>
+              <div className='control is-expanded'>
+                <div className='select is-fullwidth'>
+                  <select
+                    onChange={(e) => setOrdersUnit(e.target.value)}
+                    value={ordersUnit}
+                  >
+                    <option value='Carton'>Carton</option>
+                    <option value='Kg'>Kg</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className='field'>
+              <label className='label'>Customer orders:</label>
               <div className='control'>
                 <input
                   className='input'
                   onChange={(e) => setCustomerOrders(e.target.value)}
-                  placeholder='Customer orders in kg'
+                  placeholder='Customer orders'
                   required={true}
                   type='number'
                   value={customerOrders}
@@ -83,8 +112,12 @@ const AddNew = ({
   )
 }
 
+const mapStateToProps = (state) => ({
+  selectedProduct: state.dashboardState.selectedProduct
+});
+
 const mapDispatchToProps = (dispatch) => ({
   addOrder: (order) => dispatch(addOrder(order))
 });
 
-export default connect(null, mapDispatchToProps)(AddNew);
+export default connect(mapStateToProps, mapDispatchToProps)(AddNew);
